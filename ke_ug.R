66c35bce87b4a19daa6b047255ab223adf2b2b09
Alex Miller <alex.k.miller@gmail.com>
2020-06-04 16:33:27 -0400

    100 percent

--- ke_ug.R
+++ ke_ug.R
@@ -41,50 +41,50 @@ single_vocabulary = function(row){
   return(row)
 }
 
-activity_url = paste0(
-  "https://iati.cloud/search/activity?q=(recipient_country_code:(KE UG) OR transaction_recipient_country_code:(KE UG))",
-  " AND transaction_date_iso_date:[2020-01-01T00:00:00Z TO *]",
-  "&fl=",
-  "default_currency,humanitarian,iati_identifier,reporting_org_*,title_*,",
-  "description_*,participating_org_*,activity_scope_code,recipient_country_*,",
-  "recipient_region_*,sector_*,tag_*,humanitarian_scope_*,policy_marker_*,collaboration_type_code,",
-  "default_flow_type_code,default_finance_type_code,default_aid_type_*,default_tied_status_code,capital_spend_percentage",
-  "&wt=csv&rows=5000000"
-)
-activities = fread(URLencode(activity_url))
-
-ids = activities$iati_identifier
-ids[which(ids == "NL-KVK-61172863-NL-KVK-61172863-DGGF I&P Afrique Enterpreneurs II")] = "NL-KVK-61172863-NL-KVK-61172863-DGGF I"
-activity_filter_vec = paste0('"',ids,'"')
-
-transaction_list = list()
-transaction_index = 1
-i = 1
-while(i < length(activity_filter_vec)){
-  start_index = i
-  end_index = min(i + 99, length(activity_filter_vec))
-  activity_filter = paste0(activity_filter_vec[start_index:end_index],collapse=" ")
-
-  transaction_url = paste0(
-    "https://iati.cloud/search/transaction?q=",
-    "iati_identifier: (",activity_filter,")",
-    " AND transaction_date_iso_date:[2020-01-01T00:00:00Z TO *]",
-    "&fl=",
-    "iati_identifier,transaction_ref,transaction_humanitarian,transaction_type_code,",
-    "transaction_date_iso_date,transaction_valu*,transaction_provider_org_*,",
-    "transaction_receiver_org_*,transaction_disbursement_channel_code,",
-    "transaction_sector_*,transaction_recipient_*,transaction_flow_type_code,",
-    "transaction_finance_type_code,transaction_aid_type_*,transaction_tied_status_code",
-    "&wt=csv&rows=5000000"
-  )
-  transactions_tmp = fread(URLencode(transaction_url))
-  transaction_list[[transaction_index]] = transactions_tmp
-  transaction_index = transaction_index + 1
-
-  i = i + 100
-}
-
-transactions = rbindlist(transaction_list)
+# activity_url = paste0(
+#   "https://iati.cloud/search/activity?q=(recipient_country_code:(KE UG) OR transaction_recipient_country_code:(KE UG))",
+#   " AND transaction_date_iso_date:[2020-01-01T00:00:00Z TO *]",
+#   "&fl=",
+#   "default_currency,humanitarian,iati_identifier,reporting_org_*,title_*,",
+#   "description_*,participating_org_*,activity_scope_code,recipient_country_*,",
+#   "recipient_region_*,sector_*,tag_*,humanitarian_scope_*,policy_marker_*,collaboration_type_code,",
+#   "default_flow_type_code,default_finance_type_code,default_aid_type_*,default_tied_status_code,capital_spend_percentage",
+#   "&wt=csv&rows=5000000"
+# )
+# activities = fread(URLencode(activity_url))
+# 
+# ids = activities$iati_identifier
+# ids[which(ids == "NL-KVK-61172863-NL-KVK-61172863-DGGF I&P Afrique Enterpreneurs II")] = "NL-KVK-61172863-NL-KVK-61172863-DGGF I"
+# activity_filter_vec = paste0('"',ids,'"')
+# 
+# transaction_list = list()
+# transaction_index = 1
+# i = 1
+# while(i < length(activity_filter_vec)){
+#   start_index = i
+#   end_index = min(i + 99, length(activity_filter_vec))
+#   activity_filter = paste0(activity_filter_vec[start_index:end_index],collapse=" ")
+# 
+#   transaction_url = paste0(
+#     "https://iati.cloud/search/transaction?q=",
+#     "iati_identifier: (",activity_filter,")",
+#     " AND transaction_date_iso_date:[2020-01-01T00:00:00Z TO *]",
+#     "&fl=",
+#     "iati_identifier,transaction_ref,transaction_humanitarian,transaction_type_code,",
+#     "transaction_date_iso_date,transaction_valu*,transaction_provider_org_*,",
+#     "transaction_receiver_org_*,transaction_disbursement_channel_code,",
+#     "transaction_sector_*,transaction_recipient_*,transaction_flow_type_code,",
+#     "transaction_finance_type_code,transaction_aid_type_*,transaction_tied_status_code",
+#     "&wt=csv&rows=5000000"
+#   )
+#   transactions_tmp = fread(URLencode(transaction_url))
+#   transaction_list[[transaction_index]] = transactions_tmp
+#   transaction_index = transaction_index + 1
+# 
+#   i = i + 100
+# }
+# 
+# transactions = rbindlist(transaction_list)
 
 # save(activities,transactions,file="ke_ug.RData")
 load("ke_ug.RData")
@@ -109,6 +109,7 @@ transactions.split.long[ , `:=`( max_count = .N , count = 1:.N, sum_percent=sum(
 transactions.split.long=subset(transactions.split.long, !is.na(generic.recipient.country.code) | max_count==1 | count==1)
 transactions.split.long$generic.recipient.country.code = as.character(transactions.split.long$generic.recipient.country.code)
 transactions.split.long$generic.recipient.country.code[which(is.na(transactions.split.long$generic.recipient.country.code))] = "Missing"
+transactions.split.long$sum_percent[which(transactions.split.long$sum_percent==0)] = 100
 transactions.split.long$country.split.transaction.value=(transactions.split.long$sum_percent/100)*transactions.split.long$transaction.value
 transactions.split.long$country.split.transaction.value[which(is.na(transactions.split.long$country.split.transaction.value))] = transactions.split.long$transaction.value[which(is.na(transactions.split.long$country.split.transaction.value))]
 transactions.split.long[,c("max_count", "count", "transaction.id", "id", "time", "sum_percent")] = NULL
@@ -138,10 +139,12 @@ transactions.split.long[ , `:=`( max_count = .N , count = 1:.N , sum_percent=sum
 transactions.split.long=subset(transactions.split.long, !is.na(generic.sector.code) | max_count==1 | count==1)
 transactions.split.long$generic.sector.code = as.character(transactions.split.long$generic.sector.code)
 transactions.split.long$generic.sector.code[which(is.na(transactions.split.long$generic.sector.code))] = "99810"
+transactions.split.long$sum_percent[which(transactions.split.long$sum_percent==0)] = 100
 transactions.split.long$sector.country.split.transaction.value=(transactions.split.long$sum_percent/100)*transactions.split.long$country.split.transaction.value
 transactions.split.long$sector.country.split.transaction.value[which(is.na(transactions.split.long$sector.country.split.transaction.value))] = transactions.split.long$country.split.transaction.value[which(is.na(transactions.split.long$sector.country.split.transaction.value))]
 transactions.split.long[,c("max_count", "count", "transaction.id", "id", "time", "sum_percent")] = NULL
 
 transactions = transactions.split.long 
 names(transactions) = gsub(".","_",names(transactions),fixed=T)
+transactions = subset(transactions, generic_recipient_country_code %in% c("UG","KE"))
 fwrite(transactions,"ke_ug.csv")
