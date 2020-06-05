447f51fd03e6f2c2e90fa3a145e2b1f43bbf8932
Alex Miller <alex.k.miller@gmail.com>
2020-06-05 10:38:20 -0400

    All vocabularies

--- ke_ug.R
+++ ke_ug.R
@@ -5,41 +5,41 @@ lapply(list.of.packages, require, character.only=T)
 
 setwd("~/git/IATI-Covid/output")
 
-single_vocabulary = function(row){
-  codes = as.character(row$generic_sector_code)
-  percentages = as.character(row$generic_sector_percentage)
-  vocabularies = as.character(row$generic_sector_vocabulary)
-  
-  code_split = str_split(codes,",")[[1]]
-  if(length(code_split)==1 & length(percentages)==0){
-    percentages = "100"
-  }
-  perc_split = str_split(percentages,",")[[1]]
-  vocab_split = str_split(vocabularies,",")[[1]]
-  if(length(code_split)!=length(perc_split) |
-     length(perc_split)!=length(vocab_split) |
-     length(vocab_split)!=length(code_split)
-  ){
-    row$generic_sector_code = ""
-    row$generic_sector_percentage = ""
-    row$generic_sector_vocabulary = ""
-    return(row)
-  }
-  row_df = data.frame(code=code_split,percent=perc_split,vocab=vocab_split)
-  if("1" %in% vocab_split){
-    row_df = subset(row_df,vocab=="1")
-  }else if("2" %in% vocab_split){
-    row_df = subset(row_df,vocab=="2")
-  }else if("DAC" %in% vocab_split){
-    row_df = subset(row_df,vocab=="DAC")
-  }else{
-    row_df = subset(row_df,is.na(vocab))
-  }
-  row$generic_sector_code = paste0(row_df$code,collapse=",")
-  row$generic_sector_percentage = paste0(row_df$percent,collapse=",")
-  row$generic_sector_vocabulary = paste0(row_df$vocab,collapse=",")
-  return(row)
-}
+# single_vocabulary = function(row){
+#   codes = as.character(row$generic_sector_code)
+#   percentages = as.character(row$generic_sector_percentage)
+#   vocabularies = as.character(row$generic_sector_vocabulary)
+#   
+#   code_split = str_split(codes,",")[[1]]
+#   if(length(code_split)==1 & length(percentages)==0){
+#     percentages = "100"
+#   }
+#   perc_split = str_split(percentages,",")[[1]]
+#   vocab_split = str_split(vocabularies,",")[[1]]
+#   if(length(code_split)!=length(perc_split) |
+#      length(perc_split)!=length(vocab_split) |
+#      length(vocab_split)!=length(code_split)
+#   ){
+#     row$generic_sector_code = ""
+#     row$generic_sector_percentage = ""
+#     row$generic_sector_vocabulary = ""
+#     return(row)
+#   }
+#   row_df = data.frame(code=code_split,percent=perc_split,vocab=vocab_split)
+#   if("1" %in% vocab_split){
+#     row_df = subset(row_df,vocab=="1")
+#   }else if("2" %in% vocab_split){
+#     row_df = subset(row_df,vocab=="2")
+#   }else if("DAC" %in% vocab_split){
+#     row_df = subset(row_df,vocab=="DAC")
+#   }else{
+#     row_df = subset(row_df,is.na(vocab))
+#   }
+#   row$generic_sector_code = paste0(row_df$code,collapse=",")
+#   row$generic_sector_percentage = paste0(row_df$percent,collapse=",")
+#   row$generic_sector_vocabulary = paste0(row_df$vocab,collapse=",")
+#   return(row)
+# }
 
 # activity_url = paste0(
 #   "https://iati.cloud/search/activity?q=(recipient_country_code:(KE UG) OR transaction_recipient_country_code:(KE UG))",
@@ -110,6 +110,7 @@ transactions.split.long=subset(transactions.split.long, !is.na(generic.recipient
 transactions.split.long$generic.recipient.country.code = as.character(transactions.split.long$generic.recipient.country.code)
 transactions.split.long$generic.recipient.country.code[which(is.na(transactions.split.long$generic.recipient.country.code))] = "Missing"
 transactions.split.long$sum_percent[which(transactions.split.long$sum_percent==0)] = 100
+transactions.split.long$generic.recipient.country.percentage[which(is.na(transactions.split.long$generic.recipient.country.percentage))] = transactions.split.long$sum_percent[which(is.na(transactions.split.long$generic.recipient.country.percentage))]
 transactions.split.long$country.split.transaction.value=(transactions.split.long$generic.recipient.country.percentage/transactions.split.long$sum_percent)*transactions.split.long$transaction.value
 transactions.split.long$country.split.transaction.value[which(is.na(transactions.split.long$country.split.transaction.value))] = transactions.split.long$transaction.value[which(is.na(transactions.split.long$country.split.transaction.value))]
 transactions.split.long[,c("max_count", "count", "transaction.id", "id", "time", "sum_percent")] = NULL
@@ -124,9 +125,9 @@ transactions$generic_sector_percentage[which(is.na(transactions$generic_sector_c
 transactions$generic_sector_vocabulary[which(is.na(transactions$generic_sector_code))] = transactions$sector.vocabulary[which(is.na(transactions$generic_sector_code))]
 transactions$generic_sector_code[which(is.na(transactions$generic_sector_code))] = transactions$sector.code[which(is.na(transactions$generic_sector_code))]
 
-for(i in 1:nrow(transactions)){
-  transactions[i,] = single_vocabulary(transactions[i,])
-}
+# for(i in 1:nrow(transactions)){
+#   transactions[i,] = single_vocabulary(transactions[i,])
+# }
 
 transactions$transaction.id = c(1:nrow(transactions))
 names(transactions) = gsub("_",".",names(transactions))
@@ -135,14 +136,14 @@ transactions.split = cSplit(transactions,c("generic.sector.code", "generic.secto
 new_names = setdiff(names(transactions.split),original_names)
 transactions.split.long =reshape(transactions.split, varying=new_names, direction="long", sep="_")
 transactions.split.long$generic.sector.percentage = as.numeric(transactions.split.long$generic.sector.percentage)
-transactions.split.long[ , `:=`( max_count = .N , count = 1:.N , sum_percent=sum(generic.sector.percentage, na.rm=T)) , by = transaction.id ]
+transactions.split.long[ , `:=`( max_count = .N , count = 1:.N ) , by = transaction.id ]
 transactions.split.long=subset(transactions.split.long, !is.na(generic.sector.code) | max_count==1 | count==1)
 transactions.split.long$generic.sector.code = as.character(transactions.split.long$generic.sector.code)
 transactions.split.long$generic.sector.code[which(is.na(transactions.split.long$generic.sector.code))] = "99810"
-transactions.split.long$sum_percent[which(transactions.split.long$sum_percent==0)] = 100
-transactions.split.long$sector.country.split.transaction.value=(transactions.split.long$generic.sector.percentage/transactions.split.long$sum_percent)*transactions.split.long$country.split.transaction.value
+transactions.split.long$generic.sector.percentage[which(is.na(transactions.split.long$generic.sector.percentage))] = 100
+transactions.split.long$sector.country.split.transaction.value=(transactions.split.long$generic.sector.percentage/100)*transactions.split.long$country.split.transaction.value
 transactions.split.long$sector.country.split.transaction.value[which(is.na(transactions.split.long$sector.country.split.transaction.value))] = transactions.split.long$country.split.transaction.value[which(is.na(transactions.split.long$sector.country.split.transaction.value))]
-transactions.split.long[,c("max_count", "count", "transaction.id", "id", "time", "sum_percent")] = NULL
+transactions.split.long[,c("max_count", "count", "transaction.id", "id", "time")] = NULL
 
 transactions = transactions.split.long 
 names(transactions) = gsub(".","_",names(transactions),fixed=T)
