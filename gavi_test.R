03cf2b6902786424ccd321186cca2f2da12a0455
Alex Miller <alex.k.miller@gmail.com>
2020-06-10 10:05:55 -0400

    GAVI test

---
+++ gavi_test.R
@@ -0,0 +1,27 @@
+list.of.packages <- c("data.table", "anytime", "dplyr", "reshape2","splitstackshape","stringr")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only=T)
+
+setwd("~/git/IATI-Covid/output")
+
+url = paste0(
+  'https://iati.cloud/search/activity?q=',
+  '(title_narrative:"gavi") OR ',
+  '(title_narrative:"vaccine") OR ',
+  '(title_narrative:"amc") OR ',
+  '(title_narrative:"iffim") OR ',
+  '(description_narrative:"gavi") OR ',
+  '(description_narrative:"vaccine") OR ',
+  '(description_narrative:"amc") OR ',
+  '(description_narrative:"iffim") OR ',
+  '(transaction_receiver_org_narrative:"gavi") OR ',
+  '(participating_org_narrative:"gavi") OR ',
+  'transaction_receiver_org_ref:(47122) OR participating_org_ref:("47122")',
+  "&fl=",
+  "iati_identifier,reporting_org_*,title*,description*,transaction_receiver*,participating_org*",
+  '&wt=xslt&tr=activity-csv.xsl&rows=5000000'
+)
+activities = read.csv(URLencode(url),as.is=T,na.strings="")
+activities = subset(activities,reporting_org_ref!="47122")
+fwrite(activities, "gavi_test.csv")
